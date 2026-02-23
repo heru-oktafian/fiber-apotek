bcd8adf0026b2047b21f29dec893ddc489471fa0
Heru Oktafian <heru.oktafian@yahoo.com>
2026-02-23 16:44:58 +0700

    Update routes of dashboard

--- routes/sys_dashboard_route.go
+++ routes/sys_dashboard_route.go
@@ -8,9 +8,10 @@ import (
 
 func SysDashboardRoute(app *fiber.App) {
 	app.Get("/api/dashboard/monthly-profit-report", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.MonthlyProfitReport)
-	app.Get("/api/dashboard/weekly-profit-report", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.WeeklyProfitReport)
 	app.Get("/api/dashboard/daily-profit-report", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.DailyProfitReport)
-	app.Get("/api/dashboard/top-selling-products", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.GetTopSellingProducts)
-	app.Get("/api/dashboard/least-selling-products", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.GetLeastSellingProducts)
+	app.Get("/api/dashboard/weekly-profit-report", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.WeeklyProfitReport)
+	app.Get("/api/dashboard/profit-today-by-user", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.GetDailyProfitReportByUser)
+	app.Get("/api/dashboard/top-selling-report", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.GetTopSellingProducts)
+	app.Get("/api/dashboard/least-selling-report", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.GetLeastSellingProducts)
 	app.Get("/api/dashboard/neared-report", middlewares.JWTMiddleware, middlewares.RoleMiddleware("administrator", "operator", "cashier", "finance", "superadmin"), controllers.GetExpiringProducts)
 }
